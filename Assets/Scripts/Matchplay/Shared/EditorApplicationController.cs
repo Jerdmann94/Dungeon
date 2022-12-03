29e21bce19813d1bc68bf952d2ad56aae1d55959
jacob <erdmannjacob0@gmail.com>
2022-12-03 12:09:48 -0600

    Working local and multiplay

---
+++ Assets/Scripts/Matchplay/Shared/EditorApplicationController.cs
@@ -0,0 +1,36 @@
+#if UNITY_EDITOR
+using ParrelSync;
+#endif
+using Matchplay.Shared;
+using UnityEngine;
+
+
+    ///Helps launch ParrelSynced Projects for easy testing
+    public class EditorApplicationController : MonoBehaviour
+    {
+        public ApplicationController m_Controller;
+
+
+        public void Start()
+        {
+#if UNITY_EDITOR
+
+            if (ClonesManager.IsClone())
+            {
+                var argument = ClonesManager.GetArgument();
+                if (argument == "server")
+                {
+                    Debug.Log("starting local server");
+                    m_Controller.OnParrelSyncStarted(true,"server");
+                }
+                    
+                else if (argument == "client")
+                {
+                    m_Controller.OnParrelSyncStarted(false,"client");
+                }
+            }
+            else
+                m_Controller.OnParrelSyncStarted(false, "client");
+#endif
+        }
+    }
